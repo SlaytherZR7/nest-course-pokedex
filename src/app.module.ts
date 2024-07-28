229f00ae51c47728ce78b341ff4b8d2b7a2825c6
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-07-28 18:31:22 -0500

    chore: Update Mongoose configuration to specify database name

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,9 @@ import { JoiValidationSchema } from './config/joi.validation';
       validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: 'pokemonsdb',
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
